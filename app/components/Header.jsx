3b63beb76f0413410e3b2f346fac45c2973af876
Stevens Lloyd <stevens91@gmail.com>
2025-02-16 19:58:44 +0100

    about me progress

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
 
     <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
       <a href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>connect with me <Image src={assets.right_arrow_white} alt='' className='w-4'/></a>
-      <a href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>my Resume<Image src={assets.download_icon} alt='' className='w-4'/></a>
+      <a href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>my resume<Image src={assets.download_icon} alt='' className='w-4'/></a>
 
     </div>
     </div>
