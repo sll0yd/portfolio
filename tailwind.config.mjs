e7f786e1654f40a7681c28318769cf8824dc4c30
Stevens Lloyd <stevens91@gmail.com>
2025-02-16 18:12:44 +0100

    first commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,9 +9,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lightHover: '#fc4ff',
+        darkhover: '#2a004a',
+        darkTheme: '#11001F',
       },
+      fontFamily: {
+        Outfit: ['Outfit', 'sans-serif'],
+        Ovo: ['Ovo', 'serif'],        
+      }
     },
   },
   plugins: [],
