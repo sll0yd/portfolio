41a221f6e91a12688963aef298bd6e2f99aacc72
Stevens Lloyd <stevens91@gmail.com>
2025-02-19 15:15:40 +0100

    isDarkMode done

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -32,7 +32,8 @@ const Contact = () => {
   return (
     <div
       id="contact"
-      className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'
+      className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]
+      dark:bg-none'
     >
       <h4 className="text-center mb-2 text-lg font-Ovo">Connect with me</h4>
       <h2 className="text-center text-5xl font-Ovo">Get in touch</h2>
@@ -46,14 +47,16 @@ const Contact = () => {
             type="text"
             placeholder="Enter your name"
             required
-            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30
+            dark:border-white/90"
             name='name'
           />
           <input
             type="email"
             placeholder="Enter your email"
             required
-            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30
+            dark:border-white/90"
             name='email'
           />
         </div>
@@ -61,12 +64,14 @@ const Contact = () => {
           rows="6"
           placeholder="Enter your message"
           required
-          className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-16"
+          className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-16 dark:bg-darkHover/30
+            dark:border-white/90"
           name='message'
         ></textarea>
         <button
           type="submit"
-          className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500"
+          className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500
+          dark:bg-transparent dark:border-[0.5px] dark:hover:bg-darkHover"
         >
           Submit now <Image src={assets.right_arrow_white} alt="" className="w-4" />
         </button>
